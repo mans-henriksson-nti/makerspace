801fc176856b9e228a0a88e993ff8c3973e16d7c
Fyrgeit (Måns Henriksson) <90310668+Fyrgeit@users.noreply.github.com>
2022-02-08 10:34:19 +0100

    save

--- app.js
+++ app.js
@@ -1,63 +1,79 @@
 let printers = [
     {
-        status: "Ur funktion",
+        status: 2,
         name: "Bengt",
-        type: "Prusa i3 MK3S"
+        type: "Prusa i3 MK3S",
     },
     {
-        status: "Ur funktion",
+        status: 2,
         name: "Mimmi",
-        type: "Prusa i3 MK3S"
+        type: "Prusa i3 MK3S",
     },
     {
-        status: "Ledig",
+        status: 0,
         name: "Sauron",
-        type: "Prusa i3 MK3S"
+        type: "Prusa i3 MK3S",
     },
     {
-        status: "Printar...",
+        status: 1,
         name: "Dan",
-        type: "Prusa i3 MK3S"
+        type: "Prusa i3 MK3S",
     },
     {
-        status: "Ur funktion",
+        status: 2,
         name: "Hammaren",
-        type: "Prusa i3 MK3S"
+        type: "Prusa i3 MK3S",
     },
     {
-        status: "Ledig",
+        status: 0,
         name: "Pär",
-        type: "Prusa i3 MK3S"
+        type: "Prusa i3 MK3S",
     },
     {
-        status: "Ledig",
+        status: 0,
         name: "Fredrik",
-        type: "Ultimaker 3"
+        type: "Ultimaker 3",
     },
     {
-        status: "Printar...",
+        status: 1,
         name: "Åke",
-        type: "Ultimaker 3"
+        type: "Ultimaker 3",
     },
     {
-        status: "Ur funktion",
+        status: 2,
         name: "Amanda",
-        type: "Ultimaker 2+"
+        type: "Ultimaker 2+",
     },
     {
-        status: "Ledig",
+        status: 0,
         name: "Anna",
-        type: "Ultimaker 2+"
-    }
-]
+        type: "Ultimaker 2+",
+    },
+];
 
-function displayPrinter(printers){
+function displayPrinter(printers) {
     let newPrinter = document.createElement("div");
     newPrinter.classList.add("printer");
 
     let newStatus = document.createElement("div");
     newStatus.classList.add("printerStatus");
-    newStatus.innerHTML = printers.status;
+    switch (printers.status) {
+        case 0:
+            newStatus.innerHTML = "Ledig";
+            newStatus.classList.add("green");
+            break;
+        case 1:
+            newStatus.innerHTML = "Printar...";
+            newStatus.classList.add("blue");
+            break;
+        case 2:
+            newStatus.innerHTML = "Ur funktion";
+            newStatus.classList.add("red");
+            break;
+
+        default:
+            break;
+    }
     newPrinter.append(newStatus);
 
     let newName = document.createElement("h1");
@@ -70,11 +86,11 @@ function displayPrinter(printers){
     newType.innerHTML = printers.type;
     newPrinter.append(newType);
 
-    return newPrinter
+    return newPrinter;
 }
 
 let main = document.querySelector(".gridContainer");
 
-for (let i = 0; i < printers.length; i++) {   
+for (let i = 0; i < printers.length; i++) {
     main.append(displayPrinter(printers[i]));
 }
